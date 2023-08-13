1a081c1aad961540375264e27e041b20840c7319
Nasir <nasir-islam@hotmail.com>
2023-08-13 23:53:23 +0100

    make searchbar placeholder more descriptive

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -33,7 +33,7 @@ const App: FC<any> = () => {
         <img src={turnitinLogo} alt='logo' />
         <div className='user-inputs'>
           <Button color='primary' className='fetch-btn' onClick={loadMemberships}>Fetch Memberships</Button>
-          <Input type='text' placeholder='Search' onChange={updateSearch} />
+          <Input type='text' placeholder='Filter by name/email' onChange={updateSearch} />
         </div>
         {
           memberships?.length > 0 && (
